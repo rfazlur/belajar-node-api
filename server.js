72aa8b4683de3a96ae4babee43d34ebd17d51fcc
Fazlur Rahman <fazlur@femaledaily.com>
2025-04-24 19:44:13 +0700

    add endpoint update user

--- server.js
+++ server.js
@@ -6,22 +6,23 @@ const app = express()
 app.use(express.json())
 
 // routes
-app.get('/', (req, res) => {
-    res.send('Hello from NODE API application')
-})
-
-app.listen(3000, ()=> {
-    console.log('Node API app is running on port 3000');
-})
- 
-app.get('/ping', (req, res) => {
-    res.send('PONG')
-})
-
 app.get('/users', async(req, res) => {
     try {
         const users = await User.find({})
         res.status(200).json(users)
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({
+            message: error.message
+        })
+    }
+})
+
+app.get('/user/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        const user = await User.findById(id)
+        res.status(200).json(user)
     } catch (error) {
         console.log(error.message)
         res.status(500).json({
@@ -36,6 +37,23 @@ app.post('/user', async(req, res) => {
         res.status(200).json(user)
     } catch(error) {
         console.log(error.message) 
+        res.status(500).json({
+            message: error.message
+        })
+    }
+})
+
+app.put('/user/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        const user = await User.findByIdAndUpdate(id, req.body)
+        if(!user) {
+            return res.status(404).json({message: `Cannot update user with id ${id}`})
+        }
+        const update_user = await User.findById(id)
+        res.status(200).json(update_user)
+    } catch (error) {
+        console.log(error.message)
         res.status(500).json({
             message: error.message
         })
