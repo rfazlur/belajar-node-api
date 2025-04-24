824b5c69295708d2956691d47ac6dff93bae125c
Fazlur Rahman <fazlur@femaledaily.com>
2025-04-24 13:01:44 +0700

    add connection to mongodb

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 // routes
@@ -6,6 +7,17 @@ app.get('/', (req, res)=> {
     res.send('Hello from NODE API application')
 })
 
-app.listen(3000, ()=> {
-    console.log('Node API app is running on port 3000');
-}) 
+app.get('/ping', (req, res)=> {
+    res.send('PONG')
+})
+
+mongoose
+.connect('mongodb+srv://admin:S7QQVBNrplWHaQtD@azkacluster.lxklmww.mongodb.net/node-api?retryWrites=true&w=majority&appName=AzkaCluster')
+.then(() => {
+    console.log('Connected to mongoDB')
+    app.listen(3000, ()=> {
+        console.log('Node API app is running on port 3000');
+    })
+}).catch((error) => {
+    console.log(error)
+})
